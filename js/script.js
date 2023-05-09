dbaba0668fbcb578e2c4d84620a4e483443fe1ef
MinhHang <mgradetsky@wpi.edu>
2023-05-09 15:33:03 -0400

    added the about and skills sections

--- js/script.js
+++ js/script.js
@@ -1,7 +1,59 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('#slides').superslides({
         animation: 'fade',
         play: 5000,
         pagination: false
     });
+
+    var typed = new Typed('.typed', {
+        strings: ['Software Engineer.', 'Web Developer.', 'Student.'],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false,
+        loopCount: Infinity
+    });
+
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            938: {
+                items: 4
+            }
+        }
+    })
+
+    var skillsTopOffset = $(".skillsSection").offset().top;
+
+    // when window is scrolled, this code will be executed
+    $(window).scroll(function(){
+        if (window.pageYOffset > skillsTopOffset - $(window).height() + 200){
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent){
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+        }
+    })
+
 });
+
+
+
