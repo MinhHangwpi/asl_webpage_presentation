71e5c6c7a913bebdcfa5980538bbb2417f47f667
MinhHang <mgradetsky@wpi.edu>
2023-05-12 20:38:27 -0400

    added loader

---
+++ assets/js/script.js
@@ -0,0 +1,125 @@
+$(window).on("load", function(){
+    $(".loader .inner").fadeOut(500, function(){
+        $(".loader").fadeOut(750);
+    });
+})
+
+$(document).ready(function () {
+    $('#slides').superslides({
+        animation: 'fade',
+        play: 5000,
+        pagination: false
+    });
+
+    var typed = new Typed('.typed', {
+        strings: ['Software Engineer.', 'Web Developer.', 'Student.'],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false,
+        loopCount: Infinity
+    });
+
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            938: {
+                items: 4
+            }
+        }
+    })
+
+    var skillsTopOffset = $(".skillsSection").offset().top;
+    var statsTopOffset = $(".statsSection").offset().top;
+    var countUpFinished = false;
+
+    // when window is scrolled, this code will be executed
+    $(window).scroll(function(){
+        if (window.pageYOffset > skillsTopOffset - $(window).height() + 200){
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent){
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+        }
+
+        if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200){
+            $(".counter").each(function(){
+                var element = $(this);
+                var endVal = parseInt(element.text());
+                element.countup(endVal);
+            })
+
+            countUpFinished = true;
+        }
+    })
+
+    // fancy box
+    $("[data-fancybox]").fancybox(); // select all elements with the [data-fancybox] attribute
+
+
+    // isotope
+
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',   
+            queue: false
+        }
+    });
+
+    $("#filters a").click(function(){
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+        
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+        return false; //override the default outcome of clicking
+    })
+
+    // make the navbar stick to the top
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation(){
+        // when we scroll the window, we will call this function
+        var body = $("body");
+        if ($(window).scrollTop() >= navTop) {
+            body.addClass("fixedNav");
+        } else {
+            body.removeClass("fixedNav");
+        }
+    }
+});
+
+
+
